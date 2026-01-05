167e0ee7c2cc533e45608d3e970a8815732454da
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 12:12:15 -0800

    Refactor maxDepth method and add author comment
    Removed the 'self' parameter from maxDepth method and added author comment.

--- maxdepth.py
+++ maxdepth.py
@@ -1,11 +1,12 @@
-# Definition for a binary tree node.
+# Omer Subasi
+
 class TreeNode:
    def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
       
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+    def maxDepth(root):
         def dfs(node, depth):
             if not node:
                 return depth
