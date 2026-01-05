7f64ba496339907521d86c85c37aa3243b7d2767
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 12:16:02 -0800

    Remove docstring from reverseString function
    Removed docstring from reverseString function.

---
+++ reversestringinplace.py
@@ -0,0 +1,10 @@
+def reverseString(s):
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            t = s[l]
+            s[l] = s[r]
+            s[r] = t
+            l += 1
+            r -= 1
+        return s
