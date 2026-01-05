4bc86a400478b6e184f604d31e76d512afc8c996
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 12:03:23 -0800

    Fix indentation in maxDepth method

--- maxdepth.py
+++ maxdepth.py
@@ -4,7 +4,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-class Solution:
+      
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def dfs(node, depth):
             if not node:
