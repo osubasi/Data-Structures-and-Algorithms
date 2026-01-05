90aa8f66889b1215b9d6fd147f1cdf5ee5d7fecc
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 12:07:56 -0800

    Add reversePrefix function in reverseprefix.py
    Implement reversePrefix function to reverse substring up to a given character.

---
+++ reverseprefix.py
@@ -0,0 +1,16 @@
+def reversePrefix(self, word: str, ch: str) -> str:
+        res = ''
+        idx = -1
+        for i in range(len(word)):
+            if word[i] == ch:
+                pre = word[:i+1]
+                j =  i
+                idx = i
+                while j >= 0:
+                    res += pre[j]
+                    j -= 1
+                break
+        
+        if idx == -1:
+            return word
+        else: return res+word[idx+1:]
