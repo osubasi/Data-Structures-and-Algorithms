c4312af42c6ae5df2218feca42e33f79b03fbb98
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 18:16:50 -0800

    Create concat.py

---
+++ concat.py
@@ -0,0 +1,10 @@
+# Omer Subasi
+
+def getConcatenation(nums):
+    n = len(nums)
+    ans = [0] * 2*n
+    for i in range(n):
+        ans[i] = nums[i]
+        ans[i + n] = nums[i]
+    
+    return ans
