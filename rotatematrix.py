c5e3f0bbf11522b1c1032b80d31fb76c1d212023
Omer Subasi <omer.subasi87@gmail.com>
2026-01-05 18:10:57 -0800

    Create rotatematrix.py

---
+++ rotatematrix.py
@@ -0,0 +1,17 @@
+# Omer Subasi
+
+def rotate(matrix):
+    self.transpose(matrix)
+    self.reflect(matrix)
+
+def transpose(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+def reflect(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n // 2):
+            matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
