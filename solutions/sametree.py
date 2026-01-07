1164148a783302c4a14f63c9943fdca3984e76bb
Omer Subasi <omer.subasi87@gmail.com>
2026-01-06 23:02:35 -0800

    Create sametree.py

---
+++ solutions/sametree.py
@@ -0,0 +1,20 @@
+# Omer Subasi
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+      self.val = val
+      self.left = left
+      self.right = right
+
+  def isSameTree(p, q):
+      if p == q == None:
+          return True
+      if p and q and p.val != q.val:
+          return False
+      if (not p and q) or (p and not q):
+          return False
+      
+      l = self.isSameTree(p.left, q.left)
+      r = self.isSameTree(p.right, q.right)
+    
+      return l and r
